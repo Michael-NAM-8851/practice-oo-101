c060ba586584330a14dd09b6e64271d8fa664cf3
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-30 16:31:17 +0800

    feat: car use engine to determine the speed

--- src/main/java/com/oo101/Vehicle.java
+++ src/main/java/com/oo101/Vehicle.java
@@ -4,12 +4,13 @@ public class Vehicle {
     private final String name;
     private final int speed;
 
+
     public Vehicle(String name, int speed){
         this.name = name;
         this.speed = speed;
     }
 
     public String speedUp() {
-        return name + ": speed up "+ speed+" km/h";
+        return name + ": speed up "+  speed  +" km/h";
     }
 }
--- src/main/java/com/oo101/test/Main.java
+++ src/main/java/com/oo101/test/Main.java
@@ -1,12 +1,10 @@
 package com.oo101.test;
 
-import com.oo101.Car;
-import com.oo101.Driver;
-import com.oo101.Truck;
+import com.oo101.*;
 
 public class Main {
     public static void main(String[] args) {
-        Car coolCar = new Car("Cool Car", 30);
+        Car coolCar = new Car("Cool Car", new GasolineEngine());
 //        System.out.println(coolCar.speedUp());
         Truck bigTruck = new Truck("Big Truck", 10);
 //        System.out.println(bigTruck.speedUp());
@@ -16,5 +14,8 @@ public class Main {
 
         System.out.println(driver1.speedUp());
         System.out.println(driver2.speedUp());
+
+        Driver driver3 = new Driver(new Car("Cool Car (Electirc)", new ElectricEngine()));
+        System.out.println(driver3.speedUp());
     }
 }
