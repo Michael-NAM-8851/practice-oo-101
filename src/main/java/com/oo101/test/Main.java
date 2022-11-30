4cc2bb376a1a814a4318924a8e98b1a24be42ec0
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-30 16:15:27 +0800

    feat: implement Car

--- src/main/java/com/oo101/test/Main.java
+++ src/main/java/com/oo101/test/Main.java
@@ -1,7 +1,10 @@
 package com.oo101.test;
 
+import com.oo101.Car;
+
 public class Main {
     public static void main(String[] args) {
-
+        Car coolCar = new Car("Cool Car", 30);
+        System.out.println(coolCar.speedUp());
     }
 }
